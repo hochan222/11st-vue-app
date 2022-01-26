1d61811191a64f8fc5c3dd39d99766badf53e870
hochan222 <hochan049@gmail.com>
2022-01-26 16:19:36 +0900

    feat: left navigation bar
    - fetch 2 depth

--- src/components/LNB.vue
+++ src/components/LNB.vue
@@ -1,15 +1,58 @@
 <template>
-  <nav class="show">
+  <nav v-if="done" class="show">
     <div class="user">
       <a href="#">로그인</a>
       <div class="flex-space"></div>
       <div class="close-nav"></div>
     </div>
-    <div class="container"></div>
+    <div class="container">
+      <!-- 카테고리 -->
+      <div class="group catagories">
+        <h3 class="group__title">
+          카테고리
+        </h3>
+        <ul class="group__list">
+          <li v-for="(item1, index) in navigations.categories.list" :key="item1.name">
+            <div class="category-icon"></div>
+            {{ item1.name }}
+            <ul class="depth">
+              <li
+                v-for="item2 in item1.list"
+                :key="item2.name">
+                <a :href="item2.href">
+                  {{ item2.name }}
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      navigations: {},
+      done: false
+    }
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    async init() {
+      this.done = false
+      this.navigations = await this.$fetch({
+        requestName: 'navigations'
+      })
+      this.done = true
+      console.log(this.navigations)
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss">
@@ -57,6 +100,43 @@ nav {
       background-position: -261px -203px;
       background-size: 363px;
       cursor: pointer;
+    }
+  }
+  .container {
+    height: calc(100% - 164px); // user height 70px + exhibitions height 94px
+    padding: 10px 0;
+    box-sizing: border-box;
+    overflow-y: auto;
+    a {
+      color: #333;
+    }
+    .group {
+      background-color: #fff;
+      margin-bottom: 10px;
+      &:last-child {
+        margin-bottom: 0;
+      }
+      &__title {
+        padding: 14px 25px;
+        font-size: 17px;
+        font-weight: 700;
+        position: relative;
+      }
+      &__list {
+        li {
+          display: flex;
+          align-items: center;
+          box-sizing: border-box;
+          cursor: pointer;
+          a {
+            display: flex;
+            align-items: center;
+            width: 100%;
+            height: 100%;
+            box-sizing: border-box;
+          }
+        }
+      }
     }
   }
 }
