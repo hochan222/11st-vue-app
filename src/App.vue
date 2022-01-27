67b9d2317b90970acabb01a65627f4cb61e6c3b5
hochan222 <hochan049@gmail.com>
2022-01-27 16:32:53 +0900

    feat: billboards

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <LNB/>
   <RNB/>
   <TheHeader />
+  <Billboard /> 
 </template>
 
 <script>
 import LNB from '~/components/LNB'
 import RNB from '~/components/RNB'
 import TheHeader from '~/components/TheHeader'
+import Billboard from '~/components/Billboard'
 
 export default {
   components: {
     LNB, 
     RNB,
-    TheHeader
+    TheHeader,
+    Billboard
   }
 }
 </script>
