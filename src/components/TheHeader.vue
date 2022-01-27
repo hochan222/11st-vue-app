7a31cc4f4eb6431803f0351f0e4d3a40aa570f83
hochan222 <hochan049@gmail.com>
2022-01-27 16:09:28 +0900

    feat: GNB power ranking wrap

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -39,14 +39,61 @@
                 <span class="index">{{ index + 1 }}</span>
                 <span class="name">{{ rank.name }}</span>
               </a>
-              <div
-                class="open-more"
-                @click="toggleRankingWrap"></div>
-            </div>
+            </div> <!-- swiper-slide end -->
+          </div> <!-- swiper-wrapper end -->
+        </div> <!-- swiper end -->
+
+      <!-- open-more start -->
+      <div
+        class="open-more"
+        @click="toggleRankingWrap"
+      >
+      </div> <!-- open-more end -->
+        
+      <div
+        v-if="isShowRankingWrap"
+        class="ranking-wrap"
+        @click.stop="">
+        <div class="title">
+          <h3>실시간 쇼핑 검색어</h3>
+          <div class="time">
+            {{ referenceDate }} 기준
+          </div>
+          <div
+            class="close-wrap"
+            @click="toggleRankingWrap"></div>
+        </div>
+        <div class="tabs">
+          <div
+            :class="{ active: !tabIndex }"
+            class="tab"
+            @click="tabIndex = 0">
+            1~10위
+          </div>
+          <div
+            :class="{ active: tabIndex }"
+            class="tab"
+            @click="tabIndex = 1">
+            11~20위
           </div>
         </div>
-      </div>
-      <!-- ranking end -->
+        <ul class="list">
+          <li
+            v-for="(rank, index) in filteredRankings"
+            :key="rank.name">
+            <a :href="rank.href">
+              <span class="index">{{ (tabIndex * 10) + index + 1 }}</span>
+              <span class="name">{{ rank.name }}</span>
+              <span class="relative-name">{{ rank.relativeName }}</span>
+            </a>
+            <div
+              :class="rank.status"
+              class="icon"></div>
+          </li>
+        </ul>
+      </div> <!-- ranking-wrap end -->
+      
+      </div> <!-- ranking end -->
 
       <!-- user-menu start -->
       <ul class="user-menu">
@@ -80,6 +127,8 @@
 </template>
 
 <script>
+import _throttle from 'lodash/throttle'
+import dayjs from 'dayjs'
 import Swiper from 'swiper/bundle'
 import 'swiper/css/bundle'
 
@@ -94,7 +143,22 @@ export default {
         { name: '취소/반품/교환', href: 'javascript:void(0)' },
         { name: '고객센터', href: 'javascript:void(0)' },
         { name: '회원정보', href: 'javascript:void(0)' }
-      ]
+      ],
+      isShowRankingWrap: false,
+      tabIndex: 0,
+      isFixed: false,
+    }
+  },
+  computed: {
+    referenceDate () {
+      return dayjs(this.rankings.referenceDate).format('YYYY.MM.DD HH:mm')
+    },
+    filteredRankings () {
+      return this.rankings.rankings.filter((rank, index) => {
+        const start = this.tabIndex * 10
+        const end = start + 9
+        return start <= index && index <= end
+      })
     }
   },
   mounted() {
@@ -102,6 +166,10 @@ export default {
   },
   methods: {
     async init() {
+      window.addEventListener('scroll', _throttle(event => {
+        this.isFixed = window.scrollY > 120
+      }, 100))
+
       this.rankings = await this.$fetch({
         requestName: 'rankings'
       })
@@ -128,8 +196,14 @@ export default {
       })
       console.log('검색결과:', data)
     },
-    toggleRankingWrap () {
-
+    toggleRankingWrap (event) {
+      event.stopPropagation()
+      this.isShowRankingWrap = !this.isShowRankingWrap
+      if (this.isShowRankingWrap) {
+        window.addEventListener('click', () => {
+          this.isShowRankingWrap = false
+        })
+      }
     }
   }
 }
@@ -274,6 +348,124 @@ header {
         transform: rotate(45deg);
       }
     }
+    .ranking-wrap {
+      position: absolute;
+      top: 44px;
+      z-index: 2;
+      width: 410px;
+      padding: 18px 25px 25px;
+      border: 1px solid #eee;
+      border-radius: 4px;
+      box-sizing: border-box;
+      box-shadow: 0 6px 24px rgba(#000,.1);
+      background-color: #fff;
+      .title {
+        display: flex;
+        align-items: flex-end;
+        h3 {
+          font-size: 17px;
+          font-weight: 700;
+        }
+        .time {
+          margin-left: 10px;
+          font-size: 13px;
+          color: #999;
+          flex: 1;
+        }
+        .close-wrap {
+          width: 36px;
+          height: 36px;
+          background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+          background-position: -261px -203px;
+          background-size: 363px;
+          cursor: pointer;
+          margin: -6px -10px;
+          transform: scale(.6666); // from 36px to 24px
+        }
+      } // ranking-wrap title end
+      .tabs {
+        height: 40px;
+        margin: 18px 0;
+        border: 1px solid #eee;
+        box-sizing: border-box;
+        background-color: #fafafa;
+        display: flex;
+        .tab {
+          flex: 1;
+          color: #7d7d7d;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          cursor: pointer;
+          &:first-child {
+            border-right: 1px solid #eee;
+          }
+          &.active {
+            background-color: #fff;
+            font-weight: 700;
+            color: #333;
+          }
+        }
+      } // tabs end
+      .list { 
+        li {
+          position: relative;
+          margin-top: 14px;
+          &:first-child {
+            margin-top: 0;
+          }
+          a {
+            display: block;
+            height: 22px;
+            line-height: 22px;
+            text-decoration: none;
+            font-size: 15px;
+            color: #000;
+            .index {
+              font-weight: 700;
+              margin-right: 10px;
+              color: #f43142;
+              font-style: italic;
+            }
+            .relative-name {
+              margin-left: 4px;
+              font-size: 13px;
+              color: #999;
+            }
+            .icon {
+              position: absolute;
+              top: 0;
+              right: 0;
+              width: 22px;
+              height: 22px;
+              background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+              background-size: 363px;
+              &.stay {
+                background-image: none;
+                &::after {
+                  content: "";
+                  position: absolute;
+                  top: 0;
+                  bottom: 0;
+                  left: 0;
+                  right: 0;
+                  margin: auto;
+                  width: 6px;
+                  height: 2px;
+                  background: #999;
+                }
+              }
+              &.up {
+                background-position: -110px -251px;
+              }
+              &.down {
+                background-position: -58px -251px;
+              }
+            } // icon end
+          } // a end
+        } // li end
+      } // list end
+    } // ranking-wrap
   } // ranking end
   
   .user-menu {
