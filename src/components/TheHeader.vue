8ee69d202e4da244e0e259d77a51297a932a38d2
hochan222 <hochan049@gmail.com>
2022-01-27 15:43:25 +0900

    feat: user-menu

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -25,6 +25,8 @@
         >
         </div>
       </div>
+
+      <!-- ranking start -->
       <div class="ranking">
         <div ref="swiper" class="swiper">
           <div class="swiper-wrapper">
@@ -44,9 +46,35 @@
           </div>
         </div>
       </div>
-      <div class="user-menu">
+      <!-- ranking end -->
 
-      </div>
+      <!-- user-menu start -->
+      <ul class="user-menu">
+        <li class="my">
+          <a href="javascript:void(0)"></a>
+          <ul class="my__menu">
+            <li
+              v-for="item in myMenu"
+              :key="item.name">
+              <a :href="item.href">
+                {{ item.name }}
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="javascript:void(0)"></a>
+        </li>
+        <li>
+          <a href="javascript:void(0)"></a>
+        </li>
+        <li>
+          <a
+            href="javascript:void(0)"
+            @click="onNav('RNB')"></a>
+        </li>
+      </ul>
+      <!-- user-menu end -->
     </div>
   </header>
 </template>
@@ -59,7 +87,14 @@ export default {
   data() {
     return {
       searchText: '',
-      rankings: {}
+      rankings: {},
+       myMenu: [
+        { name: '나의 쿠폰', href: 'javascript:void(0)' },
+        { name: '주문/배송조회', href: 'javascript:void(0)' },
+        { name: '취소/반품/교환', href: 'javascript:void(0)' },
+        { name: '고객센터', href: 'javascript:void(0)' },
+        { name: '회원정보', href: 'javascript:void(0)' }
+      ]
     }
   },
   mounted() {
@@ -240,5 +275,64 @@ header {
       }
     }
   } // ranking end
+  
+  .user-menu {
+    display: flex;
+    > li {
+      margin-right: 25px;
+      padding: 7px 0;
+      position: relative;
+      &:last-child {
+        margin-right: 0;
+      }
+      > a {
+        display: block;
+        width: 48px;
+        height: 48px;
+        background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+        background-size: 363px;
+      }
+      &:nth-child(1) > a { background-position: -106px -145px; }
+      &:nth-child(2) > a { background-position: 0px -198px; }
+      &:nth-child(3) > a { background-position: -53px -198px; }
+      &:nth-child(4) > a { background-position: -94px -70px; }
+      &:nth-child(1) > a:hover { background-position: -53px -145px; }
+      &:nth-child(2) > a:hover { background-position: -159px -145px; }
+      &:nth-child(3) > a:hover { background-position: 0px -145px; }
+      &:nth-child(4) > a:hover { background-position: -106px -198px; }
+      &.my {
+        &:hover {
+          .my__menu {
+            display: block;
+          }
+        }
+        .my__menu {
+          display: none;
+          width: 170px;
+          padding: 15px 0;
+          position: absolute;
+          top: 60px;
+          left: 0;
+          z-index: 2;
+          border: 1px solid #eee;
+          border-radius: 6px;
+          box-sizing: border-box;
+          box-shadow: 0 6px 24px -8px rgba(#000,.12);
+          background-color: #fff;
+          li {
+            a {
+              display: block;
+              padding: 7px 10px 7px 25px;
+              font-size: 15px;
+              &:hover {
+                color: #f43142;
+                background: #fafafa;
+              }
+            }
+          }
+        }
+      }
+    }
+  } // user-menu end
 }
 </style>
